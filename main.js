a8ec13bdd84b76def87848f8768724d395e649b0
Iliana Genova <iliana.genova.02@gmail.com>
2020-03-17 01:31:12 +0200

    first hello world

---
+++ main.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://iliana:<moje>@chatappcluster-sbbmt.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  
+  client.close();
+});
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);

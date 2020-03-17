c73c1e2e6beaaf8eadda11e25c333b1bb17a3d27
Iliana Genova <iliana.genova.02@gmail.com>
2020-03-17 22:13:41 +0200

    fixing client error <

--- main.js
+++ main.js
@@ -3,13 +3,6 @@ const uri = "mongodb+srv://iliana:<moje>@chatappcluster-sbbmt.mongodb.net/test?r
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  
+
   client.close();
 });
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const io = require('socket.io')(3000)
+const fs = require('fs')
+//const express = require('express');
+//const app = express();app.set('view engine', 'ejs');
 
 io.on('connection', socket => {
   console.log("new user")
   socket.emit('chat-message', 'Hello World')
 })
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+      if(error) {
+        res.writeHead(404);
+        res.write("File not found");
+      } else {
+        res.write(data);
+      }
+    res.end();
+  })
+    //res.end('Hello World!');
+    //res.render('index');
+}).listen(8080);

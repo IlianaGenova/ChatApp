20a00398952612f5a9a350403e47b624a04008fa
pgmatev <petargabriel.matev@gmail.com>
2020-03-22 17:48:14 +0200

    get for profile and login (post still not working)

--- static/models/routes/router.js
+++ static/models/routes/router.js
@@ -34,7 +34,38 @@ router.post('/register', function (req, res, next) {
         err.status = 400;
         return next(err);
     }
+
 })
+
+router.get('/profile', function (req, res, next) {
+    User.findById(req.session.userId).exec(function (error, user) {
+        if (error) {
+            return next(error);
+        } else {
+            if (user === null) {
+                var err = new Error('Not authorized! Go back!');
+                err.status = 400;
+                return next(err);
+            } else {
+                return res.redirect('/profile');
+            }
+        }
+    });
+});
+
+router.get('/logout', function (req, res, next) {
+    if (req.session) {
+        req.session.destroy(function (err) {
+            if (err) {
+                return next(err);
+            } else {
+                return res.redirect('/register');
+            }
+        });
+    }
+});
+
+
 
 module.exports = router;
 //var exports = module.exports = {};
